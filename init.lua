10df63260b13fced703f6e66693c6ff9feba5a50
Osvaldo Cordova Aburto <ocordova@pulsarml.com>
2024-07-29 10:04:35 -0600

    feat (Plugins): Update config

--- init.lua
+++ init.lua
@@ -1,10 +1,2 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-local utils = require("utils")
-
-vim.api.nvim_set_keymap(
-  "n",
-  "<leader>jc",
-  'print(utils.find_nearest_jest_config(vim.fn.expand("%:p:h")))<CR>',
-  { noremap = true, silent = true }
-)
