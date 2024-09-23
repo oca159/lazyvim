6a2629ee5a7ded72e45854c214d4017d60cdaf4f
Osvaldo Cordova Aburto <ocordova@pulsarml.com>
2024-09-22 22:30:31 -0600

    feat (Setup): Update keymaps, dashboard, plugins

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,6 +4,7 @@
 
 local keymap = vim.keymap
 --- Increment and decrement numbers
+
 keymap.set("n", "+", "<C-a>", { desc = "Increment number" })
 keymap.set("n", "-", "<C-x>", { desc = "Decrement number" })
 
@@ -12,3 +13,18 @@ keymap.set("n", "ga", "ggVG", { desc = "Select all" })
 
 --- Exit from inser mode
 keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
+
+--- Scroll up/down
+keymap.set("n", "<C-u>", "<C-u>zz")
+keymap.set("n", "<C-d>", "<C-d>zz")
+
+--- Search
+keymap.set("n", "n", "nzzzv")
+keymap.set("n", "N", "Nzzzv")
+
+--- Move lines up/down
+keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Bettter way to join lines
+keymap.set("n", "J", "mzJ`z")
