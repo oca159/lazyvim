8d0120f090dd3c5769ff6ce7e3dc05bac77e777c
Osvaldo Cordova Aburto <ocordova@pulsarml.com>
2024-12-17 13:34:48 -0600

    feat: add venv selector

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -7,3 +7,5 @@ vim.o.tabstop = 2 -- A TAB character looks like 2 spaces
 vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
 vim.o.softtabstop = 2 -- Number of spaces inserted instead of a TAB character
 vim.o.shiftwidth = 2 -- Number of spaces inserted when indenting
+vim.g.lazyvim_python_lsp = "pyright"
+vim.g.lazyvim_python_ruff = "ruff"
